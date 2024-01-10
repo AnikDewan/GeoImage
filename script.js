5c8cb12d77ebbc45f2c216a8cf705e39bb3be0cd
AnikDewan <153647733+AnikDewan@users.noreply.github.com>
2024-01-10 12:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,399 @@
+const cpt1 = [
+	{
+		name: "ক্যানিয়ন (Canyon)",
+		id: "canyon",
+		doc: "https://en.m.wikipedia.org/wiki/Canyon",
+		images: [
+			"https://cdn1.coolworks.com/production/clients/93/pictures/25139/content/shutterstock_83175214.jpg",
+			"https://i0.wp.com/sabdekho.in/wp-content/uploads/2023/06/20230622_120939271.jpg?resize=1024%2C680&ssl=1"
+		]
+	},
+	{
+		name: "গিরিখাত (Gonrge)",
+		id: "gorge",
+		doc: "https://education.nationalgeographic.org/resource/gorge/",
+		images: [
+			"https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638888204/EducationHub/photos/gorge-hostel.jpg",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-CppkEPOKEtNaT5t-r24tVQBnjCQeqH4KY28dvQsVvBUzaV-ScaeAiw8&s=10",
+			"https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638889867/EducationHub/photos/royal-gorge-bridge-colorado.jpg",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ529E5jAU4V-3R7QPl34DNRjl4rLO6Wc3E3bIH2AHnUqeoOGjTolShmM7x&s=10"
+		]
+	},
+	{
+		name: "জলপ্রপাত (Waterfall)",
+		id: "waterfall",
+		doc: "https://bn.m.wikipedia.org/wiki/%E0%A6%9C%E0%A6%B2%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%AA%E0%A6%BE%E0%A6%A4",
+		images: [
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjpVLNMkaeq2fWqcGFJb-Hc3OcU84iwGVWJQ&usqp=CAU",
+			"https://upload.wikimedia.org/wikipedia/commons/d/db/Chitrakoot_Fall_during_summer_%28June%29.jpg",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdvvdn-pyV7GUiK4fyYf1uNhkG5zaoxpqKPQ&usqp=CAU"
+		]
+	},
+	{
+		name: "মন্থকূপ (Pot holes)",
+		id: "potholes",
+		doc: "https://en.m.wikipedia.org/wiki/Pothole_(landform)",
+		images: [
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh_rMko0s2DkabzwiuCimZe4QYtAzGjbBD5w&usqp=CAU",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgSYlp9Ka3DHnvs7HKmYzrjv8vrCIysukn2g&usqp=CAU",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT99Ai7SGKDKBgZKnsdS4s_xfi10iKNI8Faw&usqp=CAU"
+		]
+	},
+	{
+    name : "পলল ব্যজনী (Alluvial Fan)",
+    id : "alluvialfan",
+    doc : "https://en.m.wikipedia.org/wiki/Alluvial_fan",
+    images : [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIGj38oUccN0vHj6upJ2z_MFjBz2bgZoz38w&usqp=CAU",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb00ffpxLE7XHxVNBRtndOjfxJIzSpYXl63A&usqp=CAU"
+      ]
+	},
+	{
+    name : "অশ্বখুরাকৃতি হ্রদ (Oxbow Lake)",
+    id : "oxbowlake",
+    doc : "https://bn.m.wikipedia.org/wiki/%E0%A6%85%E0%A6%B6%E0%A7%8D%E0%A6%AC%E0%A6%96%E0%A7%81%E0%A6%B0%E0%A6%BE%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF_%E0%A6%B9%E0%A7%8D%E0%A6%B0%E0%A6%A6",
+    images : [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR-IndSioJaEvARmMROIGp7JMpBtxPqdMIhg&usqp=CAU",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9oZL3YOCERedAdN6wfxyipALjwLOEtSQHSQ&usqp=CAU"
+      ]
+	},
+	{
+    name : "নদী বাঁক (Meander)",
+    id : "meander",
+    doc : "https://en.m.wikipedia.org/wiki/Meander",
+    images : [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaS5-JA60bfwHrvaNFnl9tGjHWad7-mjrTqf2jNwX4Sz0dltr34kXU7nWk&s=10",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVLkFhzeY2hFp9RUdK1az1n-dxioQ3WQrFxv0wR3bvqDxPQbxUd06znKM&s=10"
+      ]
+	},
+	{
+    name : "প্লাবনভূমি (Floodplain)",
+    id : "floodplain",
+    doc : "https://bn.m.wikipedia.org/wiki/%E0%A6%AA%E0%A7%8D%E0%A6%B2%E0%A6%BE%E0%A6%AC%E0%A6%A8%E0%A6%AD%E0%A7%82%E0%A6%AE%E0%A6%BF",
+    images : [
+      "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638890285/EducationHub/photos/white-river.jpg",
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Paran%C3%A1_River_Floodplain.jpg/640px-Paran%C3%A1_River_Floodplain.jpg",
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg527b9-ImgBiVDB0huSs00gyQIZDom767At-_cjfGdEV01Fu8CsOiPIzA&s=10"
+      ]
+	},
+	{
+    name : "স্বাভাবিক বাঁধ (Levee)",
+    id : "levee",
+    doc : "https://en.m.wikipedia.org/wiki/Levee",
+    images : [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx62A3a6lCOKFk89RO6CGdzgEBT8AJDeYGig&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIsOILczJkr-a_CKoNIDeUpkMIJ8-t4g09uze2gL-u3GbFIaIqkmSK2Dw&s=10"
+      ]
+	},
+	{
+    name : "নদীর ক্ষয়কার্য",
+    id : "erosion",
+    doc : "https://bn.m.wikipedia.org/wiki/%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%AF%E0%A6%BC%E0%A7%80%E0%A6%AD%E0%A6%AC%E0%A6%A8",
+    images : [
+			"https://www.unfoldbangla.com/media/library/editor/1673324382.jpg"
+      ]
+	},
+	{
+    name : "নদীর বহন প্রক্রিয়া",
+    id : "transportation",
+    doc : "https://www.internetgeography.net/topics/how-do-rivers-transport-material/",
+    images : [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDVK9_DtzChEGbzOsF_OrWcjOphRKbgRPshA&usqp=CAU"
+      ]
+	}
+
+]
+
+const cpt2 = [
+	{
+		name: "",
+		id: "",
+		doc: "",
+		images: []
+	}
+]
+
+const cpt3 = [
+	{
+		name: "",
+		id: "",
+		doc: "",
+		images: []
+	}
+]
+
+const cpt4 = [
+	{
+		name: "",
+		id: "",
+		doc: "",
+		images: []
+	}
+]
+
+const cpt5 = [
+	{
+		name: "",
+		id: "",
+		doc: "",
+		images: []
+	}
+]
+
+const cpt6 = [
+	{
+		name: "",
+		id: "",
+		doc: "",
+		images: []
+	}
+]
+
+const dataset = [cpt1, cpt2, cpt3, cpt4, cpt5, cpt6]
+let data;
+
+const slide = (i) => {
+	let wrapper = document.getElementById(`slider${i}`),
+		items = document.getElementById(`slides${i}`),
+		prev = document.getElementById(`prev${i}`),
+		next = document.getElementById(`next${i}`);
+	let posX1 = 0,
+		posX2 = 0,
+		posInitial,
+		posFinal,
+		threshold = 100,
+		slides = items.getElementsByClassName('slide'),
+		slidesLength = slides.length,
+		slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+		firstSlide = slides[0],
+		lastSlide = slides[slidesLength - 1],
+		cloneFirst = firstSlide.cloneNode(true),
+		cloneLast = lastSlide.cloneNode(true),
+		index = 0,
+		allowShift = true;
+
+	// Clone first and last slide
+
+	items.appendChild(cloneFirst);
+	items.insertBefore(cloneLast, firstSlide);
+	wrapper.classList.add('loaded');
+
+	// Mouse events
+
+	items.onmousedown = dragStart;
+
+	// Touch events
+
+	items.addEventListener('touchstart', dragStart);
+	items.addEventListener('touchend', dragEnd);
+	items.addEventListener('touchmove', dragAction);
+
+	// Click events
+	prev.addEventListener('click', function() { shiftSlide(-1) });
+	next.addEventListener('click', function() { shiftSlide(1) });
+
+	// Transition events
+
+	items.addEventListener('transitionend', checkIndex);
+
+	function dragStart(e) {
+		e = e || window.event;
+		e.preventDefault();
+		posInitial = items.offsetLeft;
+
+		if (e.type == 'touchstart') {
+			posX1 = e.touches[0].clientX;
+		}
+		else {
+			posX1 = e.clientX;
+			document.onmouseup = dragEnd;
+			document.onmousemove = dragAction;
+		}
+	}
+
+	function dragAction(e) {
+		e = e || window.event;
+		if (e.type == 'touchmove') {
+			posX2 = posX1 - e.touches[0].clientX;
+			posX1 = e.touches[0].clientX;
+		}
+		else {
+			posX2 = posX1 - e.clientX;
+			posX1 = e.clientX;
+		}
+		items.style.left = (items.offsetLeft - posX2) + "px";
+	}
+
+	function dragEnd(e) {
+		posFinal = items.offsetLeft;
+		if (posFinal - posInitial < -threshold) {
+			shiftSlide(1, 'drag');
+		}
+		else if (posFinal - posInitial > threshold) {
+			shiftSlide(-1, 'drag');
+		}
+		else {
+			items.style.left = (posInitial) + "px";
+		}
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+
+	function shiftSlide(dir, action) {
+		items.classList.add('shifting');
+		if (allowShift) {
+			if (!action) { posInitial = items.offsetLeft; }
+			if (dir == 1) {
+				items.style.left = (posInitial - slideSize) + "px";
+				index++;
+			}
+			else if (dir == -1) {
+				items.style.left = (posInitial + slideSize) + "px";
+				index--;
+			}
+		};
+		allowShift = false;
+	}
+	function checkIndex() {
+		items.classList.remove('shifting');
+		if (index == -1) {
+			items.style.left = -(slidesLength * slideSize) + "px";
+			index = slidesLength - 1;
+		}
+		if (index == slidesLength) {
+			items.style.left = -(1 * slideSize) + "px";
+			index = 0;
+		}
+		allowShift = true;
+	}
+}
+
+const nameToData = (name) => {
+	let nameArr = name.split(" ")
+	let idArr = []
+	let NAMEArr = []
+	let regex = /^[a-zA-Z]+$/;
+
+	for ( e in nameArr){
+	  
+	  if (regex.test(nameArr[e])) {
+		  idArr.push(nameArr[e])
+	  }
+	  else {
+		  NAMEArr.push(nameArr[e])
+	  }
+  }
+  let NAME = NAMEArr.join(" ")
+	let id = idArr.join(" ")
+  return [NAME, id]
+}
+		
+const cardShow = (index) => {
+	let allCards = document.getElementById("allCards")
+	data = dataset[index]
+	allCards.innerHTML = ""
+	for (d in data) {   
+		let ihtml = ""
+
+		ihtml += `
+	  <div class="cards" id = ${data[d].id}>
+      <h2><a href = "${data[d].doc}">${data[d].name}</a></h2>
+      <div id="slider${d}" class="slider">
+        <div class="wrapper">
+          <div id="slides${d}" class="slides">`
+
+		for (img in data[d].images) {
+			ihtml += `<span class="slide"><img src="${data[d].images[img]}" alt="Image not found"></span>`
+		}
+
+		ihtml += `</div>
+        </div>
+      </div>
+
+			<div class="imgScroll">
+					<button class="btn btn-secondary" id="prev${d}"><i class="fa-solid fa-arrow-left fa-beat-fade"></i></button>
+				<button class="btn btn-secondary" id="next${d}"><i class="fa-solid fa-arrow-right fa-beat-fade"></i></button>
+			</div>
+		
+			</div>`
+		allCards.innerHTML += ihtml
+	}
+
+	for (d in data) {
+		slide(d)
+	}
+}
+
+const select = (val) => {
+	let index = val.dataset.index
+	let selected = document.getElementById("selected")
+	selected.innerHTML = `Chapter ${index}`
+	selected.style.animation = "searchShow 1s ease-in-out forwards"
+
+	let search = document.getElementById("search")
+	search.style.display = "block"
+
+	cardShow(parseInt(index) - 1)
+}
+
+const SEARCH = (val) => {
+	let suggestion = document.getElementById("suggestion")
+	let ihtml = ""
+	if (val.length > 1) {
+		suggestion.style.display = "flex"
+		for (d in data) {
+			if (data[d].name.toUpperCase().indexOf(val.toUpperCase()) > -1) {
+				ihtml += `<a href=#${data[d].id}>${data[d].name}</a>`
+			}
+		}
+	}
+
+	else {
+		suggestion.style.display = "none"
+	}
+	if (ihtml != "") {
+		suggestion.innerHTML = ihtml
+	}
+	else {
+		suggestion.innerHTML = "NO SEARCH RESULT FOUND"
+	}
+}
+
+const sendEmail = () => {
+	let chapter = document.getElementById("chapter")
+	let name = document.getElementById("name")
+	let doc = document.getElementById("doc")
+	let images = document.getElementById("images")
+  let [NAME , ID] = nameToData(name.value)
+  let Images = images.value.split(",").join('","')
+
+	function camelCase(str) {
+    let ans = str.toLowerCase();
+    return ans.split(" ").reduce((s, c) => s
+        + (c.charAt(0).toUpperCase() + c.slice(1)));
+		}
+	
+  if (ID){
+		NAME = `${NAME} (${ID})`
+	  ID = camelCase(ID)
+  }
+	else {
+		ID = Date.now()
+	}
+
+	Email.send({
+		Host: "smtp.elasticemail.com",
+		Username: "smtpuser0000@gmail.com",
+		Password: "CC674ECD2A32DBA5562AB9805BC3704999D3",
+		To: 'smtpuser0000@gmail.com',
+		From: "smtpuser0000@gmail.com",
+		Subject: "Contributions",
+		Body: `chapter${chapter.value} = {
+		name : "${NAME}",
+    id : "${ID}",
+		doc : "${doc.value}",
+    images : ["${Images}"]}`
+	}).then(
+		message => alert("Thank you for your contribution")
+	);
+}
